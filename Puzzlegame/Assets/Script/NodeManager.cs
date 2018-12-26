7e7bd0bfe3f70e5833225642cad84e8b54b64ac7
LeeYonggi <rl4459@naver.com>
2018-12-26 20:32:42 +0900

    Basic

---
+++ Puzzlegame/Assets/Script/NodeManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeManager : MonoBehaviour {
+
+    private Swipe m_Swipe;
+    public GameObject testobj;
+    private Vector3 desiredPosition;
+
+	// Use this for initialization
+	void Start () {
+        m_Swipe = GetComponent<Swipe>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (m_Swipe.SwipeLeft)
+            desiredPosition += Vector3.left;
+        if (m_Swipe.SwipeRight)
+            desiredPosition += Vector3.right;
+        if (m_Swipe.SwipeUp)
+            desiredPosition += Vector3.up;
+        if (m_Swipe.SwipeDown)
+            desiredPosition += Vector3.down;
+
+        testobj.transform.position = Vector3.MoveTowards(testobj.transform.position, desiredPosition, 3f * Time.deltaTime);
+
+        if (m_Swipe.Tap)
+            Debug.Log("Tapp!");
+    }
+}
